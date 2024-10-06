bf35009387a868c99290a0a31bc5e6a733912d31
naxifa <maliha1@usf.edu>
2024-10-06 19:00:21 -0400

    Create Manager class with inheritance from Employee

--- employee_management.js
+++ employee_management.js
@@ -10,7 +10,7 @@ class Employee {
     }
 getDetails() {
     return `Name: ${this.name}, Position: ${this.position}, Salary: $${this.salary}`;
-    }}
+        }}
 
 
 let _employee = new Employee ("Alice", 80000, "Developer", "Engineering");
@@ -24,19 +24,28 @@ class Department {
     constructor(name) {
         this.name = name 
         this.employees = []
-    }
+           }
     addEmployee(employee) {
     this.employees.push(employee);
-    }
+           }
 
     getDepartmentSalary() {
     return this.employees.reduce((total, emp) => total + emp.salary, 0);
+           }
     }
-}
 
 
+// Tssk 3 - Creating a Manager Class that Inherits from Employee
 
+class Manager extends Employee {
+    constructor(name, salary, position, department, bonus) {
+        super(name, salary, position, department);
+        this.bonus = bonus;
+            }
 
+    getDetails() {
+        return `Name: ${this.name}, Position: ${this.position}, Salary: $${this.salary}, Bonus: $${this.bonus}`;
+            }}
 
 
 
